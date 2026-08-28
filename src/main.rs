da65d094059cc0a6073d35a262820a22db01d05f
Rakhat Yskak <yskak.rakhat@gmail.com>
2026-08-29 01:21:34 +0500

    feat(api): checkhealth
    Signed-off-by: Rakhat Yskak <yskak.rakhat@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/checkhealth")]
+async fn checkhealth() -> impl Responder {
+    HttpResponse::Ok().body("healthy!")
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(checkhealth))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
